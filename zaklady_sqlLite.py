8ef78f6dd7de38e36073714e805fe9c852d9e786
koukalka <79112645+koukalka@users.noreply.github.com>
2021-02-16 17:53:03 +0100

    Update zaklady_sqlLite.py
    nektere ukoly

--- zaklady_sqlLite.py
+++ zaklady_sqlLite.py
@@ -14,19 +14,11 @@ SELECT - employees:
 # Vypiš všechny jména zákazníků bez opakování
 # Vypiš všechny křestní jména seřazená od A do Z
 
-SELECT * FROM customers where Company = "Microsoft Corporation"
-SELECT * FROM customers where Company = "Microsoft Corporation" or Company = "Apple Inc."
-SELECT DISTINCT FirstName FROM customers
-
 # COUNT
+# Vypiš kolik zaměstnanců jsou z Německa
+# Vypis kolik zaměstnanců pracují v Applu
 #Vypiš kolikrát se opakují daná křestní jména (seřazená od největšího po nejmenší)
-SELECT FirstName, COUNT(*) as cnt FROM customers group by 1 order by cnt desc;
 
-SELECT COUNT(*) FROM customers
-WHERE COUNTRY = 'Canada';
-
-SELECT * FROM customers
-WHERE COUNTRY = 'Canada';
 
 # https://www.sqlitetutorial.net/tryit/query/sqlite-create-table/#1
 # vytvorit tabulku
